dbf62e0cd309ee8d6304b157d43bebc41ae7baff
Olga <75717471+Folalika@users.noreply.github.com>
2022-12-25 23:45:06 +0300

    LB4
    4

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled'
+
+const FooterStyled = styled.div`
+  color: red;
+  background-color: blue;
+  text-align: left;
+`
+
+
+export const Footer = () => {
+    return (<div>
+
+        <FooterStyled>The Footer</FooterStyled>
+        
+    </div>)
+}
---
+++ components/Main/Main.jsx
@@ -0,0 +1,5 @@
+export const Main = ({ children }) => {
+    return (<div style={{ backgroundColor: 'pink', height: '100%' }}>
+        {children}
+    </div>)
+}
